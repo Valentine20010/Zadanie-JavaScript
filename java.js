28e9ee9fe3be1d63c5fac838d874e9f3b222a4d0
Valentine20010 <161027769+Valentine20010@users.noreply.github.com>
2024-02-26 16:56:42 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,54 @@
+// Wprowadź liczbę.
+
+const liczba = Number(prompt('Podaj liczbe: '))
+
+// Oblicz liczbę o przeciwnym znaku do wprowadzonej.
+
+//alert((liczba)*-1)
+
+// Oblicz sumę wprowadzonej liczby i 15.
+
+// alert((liczba)+15)
+
+// Oblicz sumę wprowadzonej liczby i 0.5.
+
+// alert((liczba)+0.5)
+
+// Oblicz kwadrat wprowadzonej liczby.
+
+// alert((liczba)*liczba)
+
+// Oblicz wartość podzielonej przez 2 wprowadzonej liczby.
+
+// alert(liczba/2)
+
+// Oblicz resztę z dzielenia wprowadzonej liczby przez 2.
+
+// alert(liczba % 2)
+
+// Oblicz liczbę o jeden większą od wprowadzonej.
+
+// alert(liczba+1)
+
+// Oblicz liczbę o jeden mniejszą od wprowadzonej.
+
+// alert(liczba-1)
+
+// Oblicz liczbę zaokrągloną w dół po podzieleniu wprowadzonej liczby przez 3.
+
+alert(liczba/3) (Math.floor(liczba));
+
+// Oblicz liczbę zaokrągloną w górę po podzieleniu wprowadzonej liczby przez 3.
+// Oblicz liczbę zaokrągloną do najbliższej całkowitej po podzieleniu wprowadzonej liczby przez 3.
+// Oblicz pierwiastek wprowadzonej liczby.
+// Oblicz iloczyn wprowadzonej liczby przez liczbę losową z przedziału od 0 do 50.
+// Oblicz wartość absolutną wprowadzonej liczby.
+// Oblicz jej podwojoną wartość.
+// Oblicz pierwiastek kwadratowy z sumy dwóch wprowadzonych liczb.
+// Wprowadź dwie liczby, a następnie oblicz różnicę ich kwadratów.
+// Wprowadź liczbę, a następnie oblicz jej potęgę trzecią.
+// Wprowadź liczbę, a następnie oblicz jej wartość podniesioną do kwadratu.
+// Wprowadź liczbę, a następnie oblicz jej pierwiastek sześcienny.
+// Oblicz średnią arytmetyczną z dwóch wprowadzonych liczb.
+// Wprowadź liczbę, a następnie oblicz jej wartość pomnożoną przez 10.
+// Oblicz sumę liczb całkowitych od 1 do wprowadzonej liczby.
